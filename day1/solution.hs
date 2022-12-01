05d388d2adf577e34507cf5f3705eb871e7ecf1d
Martin Sosic <sosic.martin@gmail.com>
2022-12-01 17:51:02 +0100

    Fixed day 1.

--- day1/solution.hs
+++ day1/solution.hs
@@ -4,8 +4,9 @@
 -}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import Data.List (sort)
+import Data.List (sort, sortOn)
 import Data.List.Split (splitOn)
+import Data.Ord (Down (Down))
 
 main :: IO ()
 main = do
@@ -17,7 +18,7 @@ solve :: String -> Int
 solve = maximum . caloriesPerElf
 
 solve2 :: String -> Int
-solve2 = sum . take 3 . caloriesPerElf
+solve2 = sum . take 3 . sortOn Down . caloriesPerElf
 
 caloriesPerElf :: String -> [Int]
 caloriesPerElf = fmap sum . (fmap . fmap) read . splitOn [""] . lines
